01c0df51b26cd24f62d0dabe5e76980e49b4da93
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-07 07:38:42 +0000

    remove the admin view victim view button from all page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -25,14 +25,6 @@ export default function DashboardLayout({
     <div className="relative flex min-h-dvh flex-col">
       <Header />
       <main className="flex-1 p-4 sm:p-6">{children}</main>
-      <div className="absolute bottom-24 right-6 z-10 sm:bottom-6 sm:right-24">
-        <Button asChild variant="outline" size="sm">
-          <Link href="/dashboard/admin">
-            <ShieldCheck className="mr-2 h-4 w-4" />
-            Admin View
-          </Link>
-        </Button>
-      </div>
       <Chatbot />
     </div>
   )
