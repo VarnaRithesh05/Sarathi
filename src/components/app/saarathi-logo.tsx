792194460117c4f079b49b70b05823934868b4fc
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 14:02:18 +0000

    Design a professional and minimalist logo icon of the Scales of Justice.

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -5,42 +5,26 @@ const SaarathiLogo = ({ className }: { className?: string }) => (
         viewBox="0 0 100 100" 
         xmlns="http://www.w3.org/2000/svg" 
         className={className || "h-12 w-auto"}
-        fill="hsl(var(--primary))"
     >
-        {/* Central Pillar and Base */}
-        <path d="M48 10 H52 V85 H48 z" />
-        <path d="M35 85 H65 V90 H35 z" />
+        <g fill="hsl(var(--primary))" stroke="hsl(var(--primary))" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
+            {/* Base and Pillar */}
+            <path d="M 35 90 H 65" fill="none" />
+            <path d="M 48 10 V 90" fill="none" />
+            <path d="M 52 10 V 90" fill="none" />
+            
+            {/* Crossbeam */}
+            <path d="M 10 10 H 90" fill="none" />
+            
+            {/* Left Hanger and Bowl */}
+            <path d="M 25 10 V 25" fill="none" />
+            <path d="M 15 40 L 25 25 L 35 40" fill="none" />
+            <path d="M 10 40 A 15 10 0 0 0 40 40" fill="none" />
 
-        {/* Crossbeam */}
-        <path d="M10 10 H90 V15 H10 z" />
-        
-        {/* Left Bowl and Strings */}
-        <g stroke="hsl(var(--primary))" strokeWidth="2.5" fill="none">
-            {/* Hanger */}
-            <path d="M25 15 V 25" />
-            {/* Bowl Connector */}
-            <path d="M10 40 H40" />
-            {/* Strings */}
-            <path d="M25 25 L 10 40" />
-            <path d="M25 25 L 40 40" />
+            {/* Right Hanger and Bowl */}
+            <path d="M 75 10 V 25" fill="none" />
+            <path d="M 65 40 L 75 25 L 85 40" fill="none" />
+            <path d="M 60 40 A 15 10 0 0 1 90 40" fill="none" />
         </g>
-        {/* Left Bowl */}
-        <path d="M5 40 A 20 15 0 0 0 45 40 Z" fill="hsl(var(--primary))" />
-
-
-        {/* Right Bowl and Strings */}
-        <g stroke="hsl(var(--primary))" strokeWidth="2.5" fill="none">
-            {/* Hanger */}
-            <path d="M75 15 V 25" />
-            {/* Bowl Connector */}
-            <path d="M60 40 H90" />
-            {/* Strings */}
-            <path d="M75 25 L 60 40" />
-            <path d="M75 25 L 90 40" />
-        </g>
-        {/* Right Bowl */}
-        <path d="M55 40 A 20 15 0 0 1 95 40 Z" fill="hsl(var(--primary))" />
-
     </svg>
 );
 
