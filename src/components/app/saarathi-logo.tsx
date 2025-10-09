df7baa7d171d7d912009660ce0ac7457d9985559
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 14:04:11 +0000

    Generate a precise and iconic logo for the Scales of Justice (Law Scale)

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -5,26 +5,27 @@ const SaarathiLogo = ({ className }: { className?: string }) => (
         viewBox="0 0 100 100" 
         xmlns="http://www.w3.org/2000/svg" 
         className={className || "h-12 w-auto"}
+        fill="hsl(var(--primary))"
     >
-        <g fill="hsl(var(--primary))" stroke="hsl(var(--primary))" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-            {/* Base and Pillar */}
-            <path d="M 35 90 H 65" fill="none" />
-            <path d="M 48 10 V 90" fill="none" />
-            <path d="M 52 10 V 90" fill="none" />
-            
-            {/* Crossbeam */}
-            <path d="M 10 10 H 90" fill="none" />
-            
-            {/* Left Hanger and Bowl */}
-            <path d="M 25 10 V 25" fill="none" />
-            <path d="M 15 40 L 25 25 L 35 40" fill="none" />
-            <path d="M 10 40 A 15 10 0 0 0 40 40" fill="none" />
+        {/* Pillar and Base */}
+        <path d="M48 10 H52 V85 A2 2 0 0 1 50 87 A2 2 0 0 1 48 85 V10z" />
+        <path d="M40 87 H60 V91 H40z" />
+        
+        {/* Crossbeam */}
+        <path d="M10 10 H90 V14 H10z" />
+        
+        {/* Central Triangle */}
+        <path d="M50 14 L55 24 H45z" />
 
-            {/* Right Hanger and Bowl */}
-            <path d="M 75 10 V 25" fill="none" />
-            <path d="M 65 40 L 75 25 L 85 40" fill="none" />
-            <path d="M 60 40 A 15 10 0 0 1 90 40" fill="none" />
-        </g>
+        {/* Left Bowl and Hanger */}
+        <path d="M22 14 V25 H18 V14z" />
+        <path d="M20 25 L15 35 L25 35z" />
+        <path d="M20 35 L5 50 A20 20 0 0 0 35 50z" />
+        
+        {/* Right Bowl and Hanger */}
+        <path d="M78 14 V25 H82 V14z" />
+        <path d="M80 25 L75 35 L85 35z" />
+        <path d="M80 35 L65 50 A20 20 0 0 1 95 50z" />
     </svg>
 );
 
