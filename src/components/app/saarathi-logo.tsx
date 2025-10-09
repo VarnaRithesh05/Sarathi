a530e300e6925b956d5e5bef344899293fad8fee
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 14:00:03 +0000

    noo change it to previous

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -5,26 +5,20 @@ const SaarathiLogo = ({ className }: { className?: string }) => (
         viewBox="0 0 100 100" 
         xmlns="http://www.w3.org/2000/svg" 
         className={className || "h-12 w-auto"}
+        fill="hsl(var(--primary))"
     >
-        <g stroke="hsl(var(--primary))" fill="hsl(var(--primary))" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-            {/* Central Pillar */}
-            <path d="M 50 85 L 50 15" />
-            {/* Base */}
-            <path d="M 35 85 L 65 85" />
-            
-            {/* Crossbeam */}
-            <path d="M 10 25 L 90 25" />
-            
-            {/* Center point of crossbeam */}
-            <path d="M 50 25 L 50 15" />
-            
-            {/* Connecting Lines (Strings) */}
-            <path d="M 25 25 L 25 50" fill="none" /> 
-            <path d="M 75 25 L 75 50" fill="none" />
-
-            {/* Bowls */}
-            <path d="M 10 50 A 15 10 0 0 0 40 50" fill="none" />
-            <path d="M 60 50 A 15 10 0 0 0 90 50" fill="none" />
+        <path d="M 48 15 L 48 85 L 52 85 L 52 15 Z" />
+        <path d="M 35 85 L 65 85 L 65 89 L 35 89 Z" />
+        <circle cx="50" cy="12" r="4" />
+        <path d="M 10 25 L 90 25 L 90 29 L 10 29 Z" />
+        <path d="M 47 23 L 53 23 L 53 31 L 47 31 Z" />
+        <g>
+            <path d="M 25 29 L 25 45" stroke="hsl(var(--primary))" strokeWidth="2" />
+            <path d="M 10 55 C 10 45, 40 45, 40 55 L 10 55 Z" />
+        </g>
+        <g>
+            <path d="M 75 29 L 75 45" stroke="hsl(var(--primary))" strokeWidth="2" />
+            <path d="M 60 55 C 60 45, 90 45, 90 55 L 60 55 Z" />
         </g>
     </svg>
 );
