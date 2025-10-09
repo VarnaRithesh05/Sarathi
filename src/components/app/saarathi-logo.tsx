944278e03ffa2feb45a1f22543497a68a558d270
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 14:05:31 +0000

    a bowlll

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -1,39 +1,35 @@
 import React from 'react';
 
 const SaarathiLogo = ({ className }: { className?: string }) => (
-    <svg 
-        viewBox="0 0 100 100" 
-        xmlns="http://www.w3.org/2000/svg" 
+    <svg
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
         className={className || "h-12 w-auto"}
-        fill="hsl(var(--primary))"
+        fill="none"
         stroke="hsl(var(--primary))"
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
     >
         {/* Pillar and Base */}
-        <path d="M50 85 V 15" fill="none" />
-        <path d="M40 90 H 60" fill="none" />
+        <path d="M50 90 V 15" />
+        <path d="M40 95 H 60" />
         
         {/* Crossbeam */}
-        <path d="M10 20 H 90" fill="none" />
+        <path d="M10 20 H 90" />
         
         {/* Central Triangle */}
-        <path d="M50 20 L 45 28 H 55 Z" fill="hsl(var(--primary))" stroke="none" />
+        <path d="M50 15 L 45 23 H 55 Z" stroke="none" fill="hsl(var(--primary))"/>
         
-        {/* Hangers */}
-        <path d="M20 20 V 30" fill="none" />
-        <path d="M80 20 V 30" fill="none" />
-        
-        {/* Left Bowl and Strings */}
-        <path d="M20 30 L 5 45" fill="none" />
-        <path d="M20 30 L 35 45" fill="none" />
-        <path d="M5 45 Q 20 60 35 45" fill="none" />
+        {/* Left Hanger and Bowl */}
+        <path d="M20 20 V 35" />
+        <path d="M10 35 L 30 35" />
+        <path d="M10 35 C 10 45, 30 45, 30 35" />
 
-        {/* Right Bowl and Strings */}
-        <path d="M80 30 L 65 45" fill="none" />
-        <path d="M80 30 L 95 45" fill="none" />
-        <path d="M65 45 Q 80 60 95 45" fill="none" />
+        {/* Right Hanger and Bowl */}
+        <path d="M80 20 V 35" />
+        <path d="M70 35 L 90 35" />
+        <path d="M70 35 C 70 45, 90 45, 90 35" />
     </svg>
 );
 
