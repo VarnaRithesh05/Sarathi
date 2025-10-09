739b2e97cdf8eeab0aca86544049320f0b9ddea3
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 14:43:33 +0000

    strings should be in a perfect angle

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -6,31 +6,30 @@ const SaarathiLogo = ({ className }: { className?: string }) => (
         xmlns="http://www.w3.org/2000/svg"
         className={className || "h-12 w-auto"}
         stroke="hsl(var(--primary))"
-        fill="hsl(var(--primary))"
+        fill="none"
         strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
     >
         {/* Pillar */}
-        <path d="M 50 25 V 85" fill="none" />
+        <path d="M 50 20 V 85" />
         {/* Base */}
-        <path d="M 35 85 H 65" fill="none" />
+        <path d="M 35 85 H 65" />
         {/* Top circle */}
-        <circle cx="50" cy="20" r="5" fill="none" />
+        <circle cx="50" cy="15" r="5" />
         
         {/* Crossbeam */}
-        <path d="M 20 35 H 80" fill="none" />
+        <path d="M 20 35 H 80" />
         
         {/* Left Bowl & Strings */}
-        <path d="M 10 55 A 15 15 0 0 0 40 55" fill="none" />
-        <path d="M 20 35 L 10 55" fill="none" />
-        <path d="M 20 35 L 40 55" fill="none" />
+        <path d="M 10 60 A 15 15 0 0 0 40 60" />
+        <path d="M 25 35 L 10 60" />
+        <path d="M 25 35 L 40 60" />
 
         {/* Right Bowl & Strings */}
-        <path d="M 60 55 A 15 15 0 0 0 90 55" fill="none" />
-        <path d="M 80 35 L 60 55" fill="none" />
-        <path d="M 80 35 L 90 55" fill="none" />
-
+        <path d="M 60 60 A 15 15 0 0 0 90 60" />
+        <path d="M 75 35 L 60 60" />
+        <path d="M 75 35 L 90 60" />
     </svg>
 );
 
