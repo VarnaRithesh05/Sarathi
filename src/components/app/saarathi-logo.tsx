82d09873eccd7c1c3b78701e1c967397a7df848b
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 13:51:04 +0000

    you could make it even better to look like a proper law scale like this

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -5,34 +5,29 @@ const SaarathiLogo = ({ className }: { className?: string }) => (
         viewBox="0 0 100 100" 
         xmlns="http://www.w3.org/2000/svg" 
         className={className || "h-12 w-auto"}
-        stroke="hsl(var(--primary))"
-        strokeWidth="5"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fill="hsl(var(--primary))"
     >
         {/* Base */}
-        <path d="M 25 90 L 75 90 L 70 95 L 30 95 Z" />
-        
-        {/* Pillar */}
-        <line x1="50" y1="90" x2="50" y2="15" />
-        
-        {/* Top decorative circle */}
-        <circle cx="50" cy="12" r="4" strokeWidth="3" />
-        
-        {/* Beam */}
-        <line x1="10" y1="35" x2="90" y2="35" />
-        
-        {/* Center pivot */}
-        <line x1="50" y1="15" x2="50" y2="35" />
+        <path d="M 30 85 L 70 85 L 70 90 L 30 90 Z" />
+        <path d="M 35 80 L 65 80 L 65 85 L 35 85 Z" />
 
+        {/* Pillar */}
+        <path d="M 45 35 L 55 35 L 55 80 L 45 80 Z" />
+        
+        {/* Top decorative piece */}
+        <path d="M 47 25 L 53 25 L 50 20 Z" />
+
+        {/* Beam */}
+        <path d="M 10 35 L 90 35 L 90 45 L 10 45 Z" />
+
+        {/* Pivot */}
+        <circle cx="50" cy="30" r="5" />
+        
         {/* Left Scale */}
-        <path d="M 15 35 L 15 45" />
-        <path d="M 5 65 C 5 50, 45 50, 45 65 Z" />
+        <path d="M 10 45 L 40 45 L 25 75 Z" />
         
         {/* Right Scale */}
-        <path d="M 85 35 L 85 45" />
-        <path d="M 55 65 C 55 50, 95 50, 95 65 Z" />
+        <path d="M 60 45 L 90 45 L 75 75 Z" />
 
     </svg>
 );
