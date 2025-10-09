0bbcccd56d2601de4bc971fffe8d83385eea1371
Varna Rithesh <varnarithu1704@gmail.com>
2025-10-09 14:30:53 +0000

    the upper line should be bit small

--- src/components/app/saarathi-logo.tsx
+++ src/components/app/saarathi-logo.tsx
@@ -17,28 +17,26 @@ const SaarathiLogo = ({ className }: { className?: string }) => (
         <path d="M 42 85 H 58" fill="none" strokeWidth="2" />
         
         {/* Crossbeam with flourishes */}
-        <path d="M 15 40 H 85" fill="none" />
-        <path d="M 12 37 L 18 43" fill="none" strokeWidth="2" />
-        <path d="M 18 37 L 12 43" fill="none" strokeWidth="2" />
-        <path d="M 82 37 L 88 43" fill="none" strokeWidth="2" />
-        <path d="M 88 37 L 82 43" fill="none" strokeWidth="2" />
+        <path d="M 25 40 H 75" fill="none" />
+        <path d="M 22 37 L 28 43" fill="none" strokeWidth="2" />
+        <path d="M 28 37 L 22 43" fill="none" strokeWidth="2" />
+        <path d="M 72 37 L 78 43" fill="none" strokeWidth="2" />
+        <path d="M 78 37 L 72 43" fill="none" strokeWidth="2" />
 
         {/* Top Circle */}
-        <circle cx="50" cy="20" r="3" fill="hsl(var(--primary))" />
+        <circle cx="50" cy="20" r="3" fill="hsl(var(--primary))" stroke="none" />
 
         {/* Curved Hangers */}
-        <path d="M 30 40 Q 20 55 15 60" fill="none" />
-        <path d="M 30 40 Q 40 55 45 60" fill="none" />
-        <path d="M 70 40 Q 80 55 85 60" fill="none" />
-        <path d="M 70 40 Q 60 55 55 60" fill="none" />
+        <path d="M 35 40 Q 25 55 30 60" fill="none" />
+        <path d="M 65 40 Q 75 55 70 60" fill="none" />
         
         {/* Bowl Separators */}
-        <path d="M 15 60 H 45" strokeWidth="2.5" fill="none" />
-        <path d="M 55 60 H 85" strokeWidth="2.5" fill="none" />
+        <path d="M 30 60 H 45" strokeWidth="2.5" fill="none" />
+        <path d="M 55 60 H 70" strokeWidth="2.5" fill="none" />
         
         {/* Filled Bowls */}
-        <path d="M 15 60 A 15 15 0 0 0 45 60 Z" />
-        <path d="M 55 60 A 15 15 0 0 0 85 60 Z" />
+        <path d="M 30 60 A 7.5 7.5 0 0 0 45 60 Z" />
+        <path d="M 55 60 A 7.5 7.5 0 0 0 70 60 Z" />
     </svg>
 );
 
