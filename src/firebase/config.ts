895aceef5b328ac1f233a5ab084fa6252405af7f
Varna Rithesh <varnarithu1704@gmail.com>
2025-11-01 12:18:56 +0530

    Modify Firebase config for new credentials
    Updated Firebase configuration with new API key and auth domain.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-9856663185-b10cd",
   "appId": "1:499514007073:web:76d25a7baa8b8a27fc4d60",
-  "apiKey": "AIzaSyCFdb6VWQKxj9VFCNUb0tl52eQndx4sq5A",
-  "authDomain": "studio-9856663185-b10cd.firebaseapp.com",
+  "apiKey": "api",
+  "authDomain": "studio-api",
   "measurementId": "",
   "messagingSenderId": "499514007073"
 };
